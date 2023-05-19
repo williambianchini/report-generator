dbfed2cc6477d09de639e0e03b1a152167ea08f3
williambianchini <contato.williambianchini@gmail.com>
2023-05-19 07:34:31 -0300

    feat(report-generator): project structure;

---
+++ src/main/java/br/com/nexfar/reportgenerator/ReportGeneratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.nexfar.reportgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportGeneratorApplication.class, args);
+	}
+
+}
